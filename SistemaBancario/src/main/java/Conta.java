ec4e6c1812f2a55cf44df6cf685f733478f4a039
unknown <dleitedias@gmail.com>
2024-07-27 11:25:13 -0300

    Task4: sistema bancário (polimorfismo)

--- SistemaBancario/src/main/java/Conta.java
+++ SistemaBancario/src/main/java/Conta.java
@@ -1,8 +1,16 @@
 public abstract class Conta implements InterfaceConta {
 
-    private int agencia;
-    private int numero;
-    private double saldo;
+    private static final int AGENCIA_PADRAO = 001;
+    private static int SEQUENCIAL = 1;
+
+    protected int agencia;
+    protected int numero;
+    protected double saldo;
+
+    public Conta() {
+        this.agencia = Conta.AGENCIA_PADRAO;
+        this.numero = SEQUENCIAL++;
+    }
 
     @Override
     public void sacar(double valor) {

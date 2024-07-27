10f4ec330b5b445bdccf80054e9de2ab69324fb0
unknown <dleitedias@gmail.com>
2024-07-27 10:41:34 -0300

    Task4: sistema bancário (abstração)

---
+++ SistemaBancario/src/main/java/ContaPoupanca.java
@@ -0,0 +1,19 @@
+public class ContaPoupanca {
+
+    public int agencia;
+    public int numero;
+    public double saldo;
+
+    public void sacar(){
+
+    }
+
+    public void depositar(){
+
+    }
+
+    public void transferir(){
+
+    }
+
+}
